0d61c982b507b24f9920903b18a8c33283e7a896
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-25 14:15:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(sourceArray, fn) {
+    let newArr = []
+    for (const int of sourceArray) {
+        newArr.push(fn(int))
+    }
+    return newArr
+}
+
+function reduce(sourceArray, fn, startingPoint=0) { 
+    let val = startingPoint
+    for (const int of sourceArray) {
+        val = fn(int)
+    }
+    return val
+}
